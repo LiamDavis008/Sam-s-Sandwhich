60e9ed5635c50220aeb432835cb15ff25ebb13d2
LiamDavis008 <liamd.22186@obhs.school.nz>
2025-10-07 13:22:15 +1300

    I added the sauce, salad, and printing to text file

--- sams_sandwhich.py
+++ sams_sandwhich.py
@@ -1,3 +1,4 @@
+import datetime
 #Sam's Sandwhich
 def bread_selection():
     bread_list =["White", "Brown","Italian","Granary"]
@@ -29,26 +30,59 @@ def cheese():
     cheese_selected=int(input("Which cheese did you want? Enter a number"))
     return cheese_list[cheese_selected-1]
 
-        
+def sauce():
+    sauce_list = ["Smorgaskaviar"]   
+    count = 0
+    print("we have the following sauce options") 
+    while count < len(sauce_list):
+        print(count+1," ", sauce_list[count])
+        count +=1
+    sauce_selected = int(input("Which cheese did you want? Enter a number"))
+    return sauce_list[sauce_selected-1]
 
-def vegies():
-    salad_list = ["None", "Açaí Palm Hearts", "Aguaje Fruit", "Amaranth Greens", "Arracacha"]
+def salad():
+    salad_list = ["Lettuce", "Tomato", "Carrot", "Cucumber", "Onions"]
     count = 0
     print("We have the following salads, you can have as many as you want")
     while count <len(salad_list):
-        print(count+1," ", salad_list[count])
-        count +=1
-    salad_selected=int(input("Which Salad did you want? Enter a number"))
-    return salad_list[salad_selected-1]
+        print(count+1, " ", salad_list[count])
+        count +=1 
+    print("press enter when you have finished choosing your salads")
+    salads_added = ""
+    selected_salad = " "
 
+    while selected_salad != "":
+        selected_salad = input(f"What number salad do you want?\nYou have selected: {salads_added}")
+        if selected_salad != "":
+            selected_salad = int(selected_salad)
+            salads_added = salads_added + " " + salad_list[selected_salad-1]
+    return salads_added.strip()
+def textfile(name, cell, today, sandwhihc_order):
+    outF=open("sams_sandwhich.txt","a")
+    print(f"\n***Order for {name} - {cell}***")
+    outF.write(f"\n Date of Order:{today}")
+    for item in sandwhich_order:
+        print(item)
+        outF.write(f"\n {item}")
+    print(f"*** END OF ORDER: ***")
+    outF.write("\n")
+    outF.write("\n")
+    outF.close()
 
 #main program
 print("welcome to Sam's Sandwhich Shop")
+name=input("what is your name?")
+cell=input("what is your phone number")
 bread_choice=bread_selection()
-print(f"Your selected bread: {bread_choice}")
 meat_choice=meats()
-print(f"Your selected the meat: {meat_choice}")
 cheese_choice=cheese()
-print(f"Your selected the cheese: {cheese}")
-salad_choice=vegies()
-print(F"Your selected Salads are: {salad_choice}")
+salad_choice=salad()
+sauce_choice=sauce()
+sandwhich_order=[]
+today=datetime.datetime.now()
+sandwhich_order.append(bread_choice)
+sandwhich_order.append(meat_choice)
+sandwhich_order.append(cheese_choice)
+sandwhich_order.append(sauce_choice)
+sandwhich_order.append(salad_choice)
+textfile(name, cell, today, sandwhich_order)
